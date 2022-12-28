b290fb5e93650196bf919501ccbc4878cc8acfd7
OksanaVorobiova <ov172608@gmail.com>
2022-12-28 12:31:13 +0200

    gallery markup and delegation in task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const makeGalleryMarkup = (galleryItems) => {
+    return galleryItems.map(({preview, original, description}) => {
+        return `<div class='gallery__item'>
+        <a class="gallery__link" href="${original}">
+            <img 
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+        </div>`
+    }).join("");
+}
+
+const galleryMarkup = makeGalleryMarkup(galleryItems);
+console.log(galleryMarkup);
+
+galleryEl.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+galleryEl.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(event) {
+    event.preventDefault();
+
+    event.target.src = event.target.dataset.source;
+}
 
 console.log(galleryItems);
